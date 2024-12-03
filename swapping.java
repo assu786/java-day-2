8818aa7e98b13fcae94e04f0b80b9673b7f287e0
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 12:12:21 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,11 @@
+class Test
+{
+    public static void main(String args[])
+    {
+        int a=50,b=43;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a +",b="+b);
+    }
+}
