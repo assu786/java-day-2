003493cb24eb8d4c3e5de219d82e52b2bf9fce56
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 12:29:36 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,15 @@
+class Bit
+{
+    public static void main(String args[])
+    {
+        int a=78;
+        if((78 & 1)==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
