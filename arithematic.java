239055a5a8ee1188d082c17f2f0fe6279c78ba70
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 10:14:46 +0530

    Add files via upload

---
+++ arithematic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class arth
+{
+    public static void main(String args[])
+    {
+    Scanner n=new Scanner(System.in);
+    int a=n.nextInt();
+    int b=n.nextInt();
+    a+=b;
+    a-=b;
+    a*=b;
+    a/=b;
+    a%=b;
+    System.out.println("a="+a);
+     }
+}     
