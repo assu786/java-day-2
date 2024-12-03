abe3853fc2ae0865eca49fd8ad48cd0e2b0a5928
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 10:58:41 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,10 @@
+class Ass
+{
+    public static void main(String args[])
+    {
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*4);
+    }
+}
