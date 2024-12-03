a9d1a59b28fd877b0aba59cf854ae4dbbfd9c16b
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 16:01:42 +0530

    Add files via upload

---
+++ leftshift.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Dd
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner (System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        System.out.println(a<<b);
+    }
+}
