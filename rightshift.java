ac13dd0ba19098123bebf09c6885c276f75129e2
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 16:15:54 +0530

    Add files via upload

---
+++ rightshift.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class S
+{
+    public static void main(String args[])
+    {
+        Scanner m=new Scanner (System.in);
+        int a=m.nextInt();
+        int b=m.nextInt();
+        System.out.println(a>>b);
+    }
+}
