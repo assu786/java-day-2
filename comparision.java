50195ffd16a7cdfe9256443d7c97b222bfa2c231
assu786 <shaikaqsa.78@gmail.com>
2024-12-03 10:22:14 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,14 @@
+class comp
+{
+    public static void main(String args[])
+    {
+        int a=76;
+        int b=56;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
