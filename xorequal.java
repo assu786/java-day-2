102082e744c9c6082e00dd0c1737bc7edcb4c363
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 11:02:12 +0530

    Add files via upload

---
+++ xorequal.java
@@ -0,0 +1,15 @@
+class H
+{
+    public static void main(String args[])
+    {
+        int a=5,b=10;
+        if((a+b)==(a^b))
+        {
+        System.out.println("equal");
+        }
+        else
+        {
+            System.out.println("not equal");
+        }
+    }
+}
